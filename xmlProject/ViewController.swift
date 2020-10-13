92f463aa5634c03790474b38891e4ddd00ba7079
Yujin Yano <northwindapps@gmail.com>
2020-10-13 11:43:05 +0900

    Initial Commit

---
+++ xmlProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xmlProject
+//
+//  Created by yujin on 2020/10/13.
+//  Copyright © 2020 yujin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
